8dbf1de0f116ad480a1507b1c5c4c0e949bcc490
Sebastian Mancke <s.mancke@tarent.de>
2016-11-27 00:07:24 +0100

    added docker option --net=host

--- README.md
+++ README.md
@@ -13,19 +13,19 @@ Mini bechmark, comparing the performance HTTP 'hello world' on different stacks:
 
 | Framework                | Req/sec (Docker)    | Req/sec (Native)
 |--------------------------|--------------------:|------------------:|
-| nginx                    | 25861               | 105844            |
-| Spring-Boot (first run)  |  6041               |  9625             |
-| Spring-Boot (second run) | 10705               | 17568             |
-| Java Spark (first run)   | 41443               | 87787             |
-| Java Spark (second run)  | 47698               | 97970             |
-| Golang                   | 60662               | 126742            |
-| NodeJS                   | 20812               | 27000             |
+| nginx                    | 105012              | 105844            |
+| Spring-Boot (first run)  |  9424               |  9625             |
+| Spring-Boot (second run) | 15739               | 17568             |
+| Java Spark (first run)   | 73393               | 87787             |
+| Java Spark (second run)  | 84323               | 97970             |
+| Golang                   | 122949              | 126742            |
+| NodeJS                   | 26628               | 27000             |
 
 
 # Test setup
 * Ubuntu 16.04.
 * i7-6600U CPU @ 2.60GHz (2 cores, with hyper threadding)
 * Tested with [wrk](https://github.com/wg/wrk) (v4.0.0): 4 threads, 20 connections, 20 seconds
-* Setup 1: All Servers are running in a docker container (v1.12.3)
-* Setup 2: Local Running Services, without docker
+* Setup 1: All Servers are running in a docker container (v1.12.3, --net=host)
+* Setup 2: Local running services
 
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker rm -f hello_nginx hello_node hello_golang springboot_hello 2> /dev/null 1> /dev/null
+docker rm -f spark_hello hello_nginx hello_node hello_golang springboot_hello 2> /dev/null 1> /dev/null
 
-pid=$(docker run -d --name spark_hello -p 8080:8080 hello_javaspark)
+pid=$(docker run --net=host -d --name spark_hello -p 8080:8080 hello_javaspark)
 sleep 1
 echo
 echo "-- HELLO Javaspark 1 --"
@@ -11,8 +11,8 @@ echo
 echo "-- HELLO Javaspark 2 --"
 wrk -c 20 -t 4 -d 20 http://127.0.0.1:8080
 docker rm -f $pid
-exit
-pid=$(docker run -d --name springboot_hello -p 8080:8080 gazgeek/springboot-helloworld)
+
+pid=$(docker run --net=host -d --name springboot_hello -p 8080:8080 gazgeek/springboot-helloworld)
 sleep 10
 echo
 echo "-- HELLO Spring-Boot 1 --"
@@ -25,15 +25,15 @@ docker rm -f $pid
 sleep 1
 echo
 echo "-- HELLO nginx --"
-pid=$(docker run -d --name hello_nginx -p 8080:80 hello_nginx)
+pid=$(docker run --net=host -d --name hello_nginx -p 80:80 hello_nginx)
 sleep 1
-wrk -c 20 -t 4 -d 20 http://127.0.0.1:8080
+wrk -c 20 -t 4 -d 20 http://127.0.0.1:80
 docker rm -f $pid
 
 sleep 1
 echo
 echo "-- HELLO node --"
-pid=$(docker run -d --name hello_node -p 8080:8080 -v $PWD:/work --workdir /work node node hello.js)
+pid=$(docker run --net=host -d --name hello_node -p 8080:8080 -v $PWD:/work --workdir /work node node hello.js)
 sleep 1
 wrk -c 20 -t 4 -d 20 http://127.0.0.1:8080
 docker rm -f $pid
@@ -41,7 +41,7 @@ docker rm -f $pid
 sleep 1
 echo
 echo "-- HELLO go --"
-pid=$(docker run -d --name hello_golang -p 8080:8080 -v $PWD:/work --workdir /work golang go run hello.go)
+pid=$(docker run --net=host -d --name hello_golang -p 8080:8080 -v $PWD:/work --workdir /work golang go run hello.go)
 sleep 1
 wrk -c 20 -t 4 -d 20 http://127.0.0.1:8080
 docker rm -f $pid
