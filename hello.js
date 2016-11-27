9dac1a468006f7d1378304ec9c25525d2107de2f
Christoph Jerolimov <jerolimov+git+2016@gmail.com>
2016-11-27 02:11:01 +0100

    Use cluster module to enable nodejs multithreading

--- hello.js
+++ hello.js
@@ -1,14 +1,28 @@
-// Load the http module to create an http server.
-var http = require('http');
+// Load the http module to create a clustered http server.
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8080);
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // Configure our HTTP server to respond with Hello World to all requests.
+  var server = http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World\n");
+  });
 
-// Put a friendly message on the terminal
-console.log("Server nodejs at http://127.0.0.1:8080/");
+  // Listen on port 8000, IP defaults to 127.0.0.1
+  server.listen(8080);
+
+  // Put a friendly message on the terminal
+  console.log("Server nodejs at http://127.0.0.1:8080/");
+}
